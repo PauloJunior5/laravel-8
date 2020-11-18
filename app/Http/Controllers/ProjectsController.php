a05398b1d242aaae22c0de90c27a0bbc3729aba4
Paulo Junior <pjunior.live@gmail.com>
2020-11-18 00:33:23 -0300

    corrections in Project model

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use App\Models\Projects;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
-class ProjectsController extends Controller
+class ProjectController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,7 +13,10 @@ class ProjectsController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::latest()->paginate(5);
+
+        return view('projects.index', compact('projects'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -24,7 +26,7 @@ class ProjectsController extends Controller
      */
     public function create()
     {
-        //
+        return view('projects.create');
     }
 
     /**
@@ -35,51 +37,99 @@ class ProjectsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'introduction' => 'required',
+            'location' => 'required',
+            'cost' => 'required'
+        ]);
+
+        Project::create($request->all());
+
+        return redirect()->route('projects.index')
+            ->with('success', 'Project created successfully.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Projects  $projects
+     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function show(Projects $projects)
+    public function show(Project $project)
     {
-        //
+        return view('projects.show', compact('project'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Projects  $projects
+     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function edit(Projects $projects)
+    public function edit(Project $project)
     {
-        //
+        return view('projects.edit', compact('project'));
     }
-
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Projects  $projects
+     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Projects $projects)
+    public function update(Request $request, Project $project)
     {
-        //
-    }
+        $request->validate([
+            'name' => 'required',
+            'introduction' => 'required',
+            'location' => 'required',
+            'cost' => 'required'
+        ]);
+        $project->update($request->all());
 
+        return redirect()->route('projects.index')
+            ->with('success', 'Project updated successfully');
+    }
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Projects  $projects
+     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Projects $projects)
+    public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route('projects.index')
+            ->with('success', 'Project deleted successfully');
     }
+}
+
+Laravel 8 has created a folder called Models, which is not available in the previous version, so our project model is found in the app/Models/Project.php, add the following functions and the fillable, the fillable are the fields in the database that a user can fill.
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $table = 'projects';
+    public $timestamps = true;
+
+    protected $casts = [
+        'cost' => 'float'
+    ];
+
+    protected $fillable = [
+        'name',
+        'introduction',
+        'created_at',
+        'location',
+        'cost'
+    ];
 }
